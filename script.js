a15fc3baf7f216ab69488bf4bb6235cc614eb4ad
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2024-07-15 20:58:31 +0200

    update

---
+++ script.js
@@ -0,0 +1,8 @@
+const buttons = document.querySelectorAll(".faq-toggle");
+console.log(buttons);
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    button.parentNode.classList.toggle("active");
+  });
+});
